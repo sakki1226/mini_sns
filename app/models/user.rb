158982b1aef046f5870b2ed133a474efb0cce3c5
sakki1226 <142205272+sakki1226@users.noreply.github.com>
2024-01-17 08:59:53 +0900

    firstcommit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+
+  has_many :sns_credentials
+
+  def self.from_omniauth(auth)
+    binding.pry
+  end
+end
